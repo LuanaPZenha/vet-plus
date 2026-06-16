d32f7af8f650d5d3e18547af2a7d27cb0fd5c5aa
Luana P Zenha <eng.luanazenha@gmail.com>
2026-06-15 21:26:07 -0300

    Redirect root URL to Swagger API docs.

--- services/auth/config/urls.py
+++ services/auth/config/urls.py
@@ -2,9 +2,11 @@
 
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="/api/docs/", permanent=False)),
     path("admin/", admin.site.urls),
     path("api/", include("src.presentation.api.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
